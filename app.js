f86d2fc9a2d9fc260eb2ead2271d8280b44affa4
Abheekrishna <abheekrishna17@gmail.com>
2023-05-20 09:54:10 +0530

    ADDED: new layout to the calculator

---
+++ app.js
@@ -0,0 +1,9 @@
+const operations = document.querySelectorAll('[data-operations]');
+const numbers = document.querySelectorAll('[data-numbers]');
+const allClear = document.querySelector('[data-all-clear]');
+const equals = document.querySelector('[data-equals]');
+const deleteBtn = document.querySelector('[data-delete]');
+const previousOperand = document.querySelector('[data-previous-operand]');
+const currentOperand = document.querySelector('[data-current-operand]');
+
+
