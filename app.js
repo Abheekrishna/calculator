587718ed5fcffbaa18cc04f6f3a2c451e505484b
Abheekrishna <abheekrishna17@gmail.com>
2023-05-20 13:19:55 +0530

    CHANGED: color of the calculator

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const operations = document.querySelectorAll('[data-operations]');
+/* 
+Steps
+
+step1: user needs to select number and operatons
+
+*/
+
+
+
+
+const operation = document.querySelectorAll('[data-operations]');
 const numbers = document.querySelectorAll('[data-numbers]');
 const allClear = document.querySelector('[data-all-clear]');
 const equals = document.querySelector('[data-equals]');
 const deleteBtn = document.querySelector('[data-delete]');
-const previousOperand = document.querySelector('[data-previous-operand]');
-const currentOperand = document.querySelector('[data-current-operand]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
 
 
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        
+    })
+})
