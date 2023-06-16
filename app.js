7edafddf1a36a222d4551b3999f8c6a8875f18e4
Abheekrishna <abheekrishna17@gmail.com>
2023-06-16 20:18:47 +0530

    Finished calculator

--- app.js
+++ app.js
@@ -12,23 +12,59 @@ class CALCULATOR {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
-        this.currentOperand = number;
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperation(operation) {
-
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
 
     compute() {
+        let result;
+        const prev = Number(this.previousOperand);
+        const curr = Number(this.currentOperand);
+        if (isNaN(prev) || isNaN(curr)) return;
 
+        switch (this.operation) {
+            case '+':
+                result = prev + curr;
+                break;
+            case '-':
+                result = prev - curr;
+                break;
+            case '*':
+                result = prev * curr;
+                break;
+            case '÷':
+                result = prev / curr;
+                break;
+            default:
+                return;
+        }
+
+        this.currentOperand = result;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
+
+
 
     updateDisplay() {
         this.currentOperandText.innerText = this.currentOperand;
+        if(this.operation != null) {
+            this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
+        }
     }
 
     
@@ -40,6 +76,10 @@ const operationBtns = document.querySelectorAll('[data-operations]');
 const equalBtn = document.querySelector('[data-equals]');
 const currentOperandText = document.querySelector('[data-current-operand]');
 const previousOperandText = document.querySelector('[data-previous-operand]');
+const allClearBtn = document.querySelector('[data-all-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+
+
 
 
 
@@ -50,4 +90,26 @@ numberBtns.forEach((button) => {
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
     })
+})
+
+operationBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalBtn.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearBtn.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteBtn.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
 })
